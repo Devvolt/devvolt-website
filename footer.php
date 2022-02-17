7b975934731c89164e8fde2f6e07beffae2231a2
Guglieee <73486489+Guglieee@users.noreply.github.com>
2022-02-17 10:51:44 +0100

    asd

--- footer.php
+++
@@ -1,5 +0,0 @@
-<script src="/node_modules/jquery/dist/jquery.js"></script>
-    <script src="/node_modules/owl.carousel/dist/owl.carousel.min.js"></script>
-    <script src="/js/main.js"></script>
-  </body>
-</html>
