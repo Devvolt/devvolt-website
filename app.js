16c7adc1d296b5f514a60e7daa77f09b925f4d68
Guglielmo Bragato <guglielmo.bragato@gmail.com>
2022-09-17 13:19:39 +0200

    Updates

--- app.js
+++ app.js
@@ -4,28 +4,14 @@ const https = require("https");
 const path = require("path");
 const fs = require("fs");
 const hsts = require("hsts");
-const axios = require("axios"); // Import Axios
-const { URLSearchParams } = require("url");
-const bodyParser = require("body-parser"); // Import body-parser
 const app = express();
-const fetch = (...args) =>
-  import("node-fetch").then(({ default: fetch }) => fetch(...args)); // Import node-fetch asynchronously; see https://www.npmjs.com/package/node-fetch#installation for more info on why this is done.
-const { Client, WebhookClient } = require("discord.js");
-const client = new Client({ intents: 32767, partials: ["CHANNEL"] });
-const {Token, webhook} = require('./config.json');
-
-client.on("ready", () => {
-  console.log("Pronto");
-});
-
-client.login(Token);
 
 //SSL
 const sslServer = https.createServer(
   {
-    key: fs.readFileSync(path.join(__dirname, "..", "cert", "private.key.pem")),
+    key: fs.readFileSync(path.join(__dirname, "..", "cert", "privkey.pem")),
     cert: fs.readFileSync(
-      path.join(__dirname, "..", "cert", "domain.cert.pem")
+      path.join(__dirname, "..", "cert", "cert.pem")
     ),
   },
   app
@@ -65,197 +51,24 @@ app.get("/portfolio/gugliee", (req, res) => {
   res.sendFile(path.join(__dirname, "portfolio", "gugliee", "index.html"));
 });
 
-// WHITELIST BORDERS
-
-/* Client Variables */
-const client_id = "955473189683744769"; // Paste your bot's ID here
-const client_secret = "bv8fgqqogY4DEtskjtUpBXkmsLbASjgw"; // Paste your bot's secret here
-
-/* Make a function to give us configuration for the Discord API */
-function make_config(authorization_token) {
-  // Define the function
-  data = {
-    // Define "data"
-    headers: {
-      // Define "headers" of "data"
-      authorization: `Bearer ${authorization_token}`, // Define the authorization
-    },
-  };
-  return data; // Return the created object
-}
-
-/* Configure the app */
-app.use(express.urlencoded({ extended: false })); // configure the app to parse requests with urlencoded payloads
-app.use(express.json()); // configure the app to parse requests with JSON payloads
-app.use(bodyParser.text()); // configure the app to be able to read text
-
-/* Handle GET Requests */
-app.get("/whitelist", (req, res) => {
-  // Handle incoming GET requests to http://localhost:(port)/
-  res.sendFile(path.join(__dirname, "whitelist", "index.html")); // Send the index.html file
-});
-app.get("/verificati", (req, res) => {
-  // Handle incoming GET requests to http://localhost:(port)/
-  res.sendFile(path.join(__dirname, "whitelist", "login.html")); // Send the index.html file
-});
-
-/* Handle POST Requests */
-app.post("/whitelist/user", (req, res) => {
-  // Will run when there are any incoming POST requests to http://localhost:(port)/user. Note that a POST request is different from a GET request, so this won't exactly work when you actually visit http://localhost:(port)/user
-  /* Create our Form Data */
-  const data_1 = new URLSearchParams(); // Create a new formData object with the constructor
-  const domande = {
-    pages: [
-      {
-        elements: [
-          {
-            type: "radiogroup",
-            name: "RDM",
-            title: "Cosa indica il termine RDM?",
-            choices: [
-              "Random Death Match",
-              "Random Dump MilanoRP",
-              "Read Death Math",
-              "Random Date Match",
-            ],
-            correctAnswer: "Radmon Death Match",
-          },
-        ],
-      },
-      {
-        elements: [
-          {
-            type: "radiogroup",
-            name: "Guglie",
-            title: "Dimmi una cosa, Guglie è un pirla?",
-            choicesOrder: "random",
-            choices: ["Si", "No", "Boh", "E' un metagamer non un pirla"],
-            correctAnswer: "E' un metagamer non un pirla",
-          },
-        ],
-      },
-    ],
-  };
-  data_1.append("client_id", client_id); // Append the client_id variable to the data
-  data_1.append("client_secret", client_secret); // Append the client_secret variable to the data
-  data_1.append("grant_type", "authorization_code"); // This field will tell the Discord API what you are wanting in your initial request.
-  data_1.append("redirect_uri", `https://devvolt.dev/whitelist`); // This is the redirect URL where the user will be redirected when they finish the Discord login
-  data_1.append("scope", "identify"); // This tells the Discord API what info you would like to retrieve. You can change this to include guilds, connections, email, etc.
-  data_1.append("code", req.body); // This is a key parameter in our upcoming request. It is the code the user got from logging in. This will help us retrieve a token which we can use to get the user's info.
-  
-
-  fetch("https://discord.com/api/v8/oauth2/token", {
-    method: "POST",
-    body: data_1,
-  })
-    .then((response) => response.json())
-    .then((data) => {
-      // Make a request to the Discord API with the form data, convert the response to JSON, then take it and run the following code.
-      axios
-        .get(
-          "https://discordapp.com/api/v8/users/@me",
-          make_config(data.access_token)
-        )
-        .then((response) => {
-          // Make a request yet again to the Discord API with the token from previously.
-          const coso = {'domande': domande, 'user': response.data}
-          res.status(200).send(coso); // Send the username with a status code 200.
-          // console.log(response.data.id)
-        })
-        .catch((err) => {
-          // Handle any errors in the request (such as 401 errors).
-          console.log(err); // Log the error in the console
-          res.sendStatus(500); // Send a 500 error.
-        });
-    });
-});
-
-app.post("/whitelist/ok", (req, res) => {
-  // Will run when there are any incoming POST requests to http://localhost:(port)/user. Note that a POST request is different from a GET request, so this won't exactly work when you actually visit http://localhost:(port)/user
-  /* Create our Form Data */
-  console.log("Test ok");
-  console.log(req.body);
-  new WebhookClient({
-    url: webhook,
-  }).send({ content: req.body });
-  // client.guilds.cache.get("914655607028613190").members.cache.get(req.body).roles.add("979995762538721331")
-});
-
 app.get("/portfolio/buffolollo", (req, res) => {
-  res.sendFile(
-    path.join(__dirname, "portfolio", "workinprogress", "index.html")
-  );
+  res.sendFile(path.join(__dirname, "portfolio", "workinprogress", "index.html"));
 });
 
 app.get("/portfolio/neonsn0w", (req, res) => {
-  res.sendFile(
-    path.join(__dirname, "portfolio", "neonsn0w", "index.html")
-  );
+  res.sendFile(path.join(__dirname, "portfolio", "neonsn0w", "index.html"));
 });
 
-app.get("/portfolio/gianmpr", (req, res) => {
-  res.sendFile(path.join(__dirname, "portfolio", "gianmpr", "index.html"));
-});
-
-app.get("/portfolio/Red_Fox", (req, res) => {
-  res.sendFile(path.join(__dirname, "portfolio", "redfox", "index.html"));
-});
-
-app.get("/portfolio/somty", (req, res) => {
-  res.sendFile(
-    path.join(__dirname, "portfolio", "workinprogress", "index.html")
-  );
-});
-
-app.get("/portfolio/Mawku", (req, res) => {
-  res.sendFile(path.join(__dirname, "portfolio", "mawku", "index.html"));
-});
-
-app.get("/portfolio/elauqzz", (req, res) => {
-  res.sendFile(path.join(__dirname, "portfolio", "elauqzz", "index.html"));
-});
-
-app.get("/portfolio/Somty", (req, res) => {
-  res.sendFile(
-    path.join(__dirname, "portfolio", "workinprogress", "index.html")
-  );
-});
-
-app.get("/ts", (req, res) => {
-  res.redirect("ts3server://borders.voicehosting.it");
-});
-
-app.get("/database", (req, res) => {
-  res.redirect("https://fdo-manager.web.app/");
-});
 
 app.get("/sitoomega", (req, res) => {
   res.redirect("http://185.229.237.130");
 });
-
 app.get("/discord", (req, res) => {
-  res.redirect("https://discord.gg/nC65WDFTUz");
+  res.redirect("https://discord.gg/UuuxbPzMsC");
 });
 
-app.get("/fivem", (req, res) => {
-  res.redirect("fivem://connect/cfx.re/join/re633x");
-});
-
-app.get("/paypal", (req, res) => {
-  res.redirect("https://paypal.me/guglie06");
-});
-app.get("/tuamadre", (req, res) => {
-  res.redirect("https://youtu.be/dQw4w9WgXcQ");
-});
-
-app.get("/portfolio/Nikuez/", (req, res) => {
-  res.sendFile(
-    path.join(__dirname, "portfolio", "workinprogress", "index.html")
-  );
-});
-
-app.get("/projects/vollex-os", (req, res) => {
-  res.sendFile(path.join(__dirname, "projects", "VollexOS", "index.html"));
+app.get("/projects/dsc-music", (req, res) => {
+  res.sendFile(path.join(__dirname, "projects", "dsc-music", "index.html"));
 });
 
 //CSS
@@ -263,9 +76,7 @@ app.get("/projects/vollex-os", (req, res) => {
 app.get("/css/style.css", (req, res) => {
   res.sendFile(path.join(__dirname, "css", "style.css"));
 });
-app.get("/css/styleborders.css", (req, res) => {
-  res.sendFile(path.join(__dirname, "css", "styleborders.css"));
-});
+
 app.get(
   "/node_modules/owl.carousel/dist/assets/owl.carousel.min.css",
   (req, res) => {
@@ -282,9 +93,6 @@ app.get(
 
 app.get("/js/main.js", (req, res) => {
   res.sendFile(path.join(__dirname, "js/main.js"));
-});
-app.get("/js/whitelist.js", (req, res) => {
-  res.sendFile(path.join(__dirname, "js/whitelist.js"));
 });
 
 app.get("/node_modules/jquery/dist/jquery.js", (req, res) => {
--- js/main.js
+++ js/main.js
@@ -1,77 +1,82 @@
-$(document).ready(function () {
-  $(window).scroll(function () {
-    // nav
-    if (this.scrollY > 20) {
-      $(".navbar").addClass("sticky");
-    } else {
-      $(".navbar").removeClass("sticky");
-    }
 
-    // pulsante di scorrimento
-    if (this.scrollY > 500) {
-      $(".scroll-up-drn").addClass("show");
-    } else {
-      $(".scroll-up-drn").removeClass("show");
-    }
-  });
-  // scorrimento
-  $(".scroll-up-drn").click(function () {
-    $("html").animate({ scrollTop: 0 });
-    // rimozione scorrimento
-    $("html").css("scrollBehavior", "auto");
-  });
-  $(".navbar .menu li a").click(function () {
-    // scroll
-    $("html").css("scrollBehavior", "smooth");
-  });
-  // alterna menu/script barra di navigazione
-  $(".menu-drn").click(function () {
-    $(".navbar .menu").toggleClass("active");
-    $(".menu-drn i").toggleClass("active");
-  });
-  $(".menu-drn").click(function () {
-    $(".navbar .menu").toggleClass("open");
-    $(".menu-drn").toggleClass("open");
-  });
-  // animazione
-  var typed = new Typed(".typing", {
-    strings: ["DEVVOLT"],
-    typeSpeed: 90,
-    backSpeed: 100,
-    startDelay: 500,
-    backDelay: 15000,
-    smartBackspace: true,
-    loop: true,
-  });
-  var typed2 = new Typed(".typing-2", {
-    strings: ["Devvolt"],
-    typeSpeed: 100,
-    backSpeed: 100,
-    startDelay: 800,
-    backDelay: 900,
-    loop: true,
-  });
-
-  // owl carousel
-  $(".carousel").owlCarousel({
-    margin: 20,
-    loop: true,
-    autoplay: true,
-    autoplayTimeOut: 2000,
-    autoplayHoverPause: true,
-    responsive: {
-      0: {
-        items: 1,
-        nav: false,
-      },
-      600: {
-        items: 2,
-        nav: false,
-      },
-      1000: {
-        items: 3,
-        nav: false,
-      },
-    },
-  });
-});
+		  $(document).ready(function () {
+        $(window).scroll(function () {
+          // nav
+          if (this.scrollY > 20) {
+            $(".navbar").addClass("sticky");
+          } else {
+            $(".navbar").removeClass("sticky");
+          }
+      
+          // pulsante di scorrimento
+          if (this.scrollY > 500) {
+            $(".scroll-up-drn").addClass("show");
+          } else {
+            $(".scroll-up-drn").removeClass("show");
+          }
+        });
+        // scorrimento
+        $(".scroll-up-drn").click(function () {
+          $("html").animate({ scrollTop: 0 });
+          // rimozione scorrimento
+          $("html").css("scrollBehavior", "auto");
+        });
+        $(".navbar .menu li a").click(function () {
+          // scroll
+          $("html").css("scrollBehavior", "smooth");
+        });
+        // alterna menu/script barra di navigazione
+        $(".menu-drn").click(function () {
+          $(".navbar .menu").toggleClass("active");
+          $(".menu-drn i").toggleClass("active");
+        });
+        $(".menu-drn").click(function () {
+          $(".navbar .menu").toggleClass("open");
+          $(".menu-drn").toggleClass("open");
+        });
+        // animazione
+        var typed = new Typed(".typing", {
+          strings: ["DEVVOLT"],
+          typeSpeed: 90,
+          backSpeed: 100,
+          startDelay: 500,
+          backDelay: 15000,
+          smartBackspace: true,
+          loop: true,
+        });
+        var typed2 = new Typed(".typing-2", {
+          strings: ["Devvolt"],
+          typeSpeed: 100,
+          backSpeed: 100,
+          startDelay: 800,
+          backDelay: 900,
+          loop: true,
+        });
+      
+        // owl carousel
+        $(".carousel").owlCarousel({
+          margin: 20,
+          loop: true,
+          autoplay: true,
+          autoplayTimeOut: 2000,
+          autoplayHoverPause: true,
+          responsive: {
+            0: {
+              items: 1,
+              nav: false,
+            },
+            600: {
+              items: 2,
+              nav: false,
+            },
+            1000: {
+              items: 3,
+              nav: false,
+            },
+            3000: {
+              items: 5,
+              nav: false,
+            },
+          },
+        });
+      });
